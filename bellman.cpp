0db5ea99b632f767a21cbb19305f93d66389e8ca
Lê Quốc Tuấn <lequoctuan.qt@gmail.com>
2025-06-01 17:35:34 +0700

    Added docstrings to functions

--- bellman.cpp
+++ bellman.cpp
@@ -1,10 +1,16 @@
 #include "bellman.h"
 
-
-int getVertexIndex(const int targetVertex, const set<int> listOfSortedVertices) {
-    if (listOfSortedVertices.empty()) return -1;
+/**
+ * Find the index of a target vertex in the set of sorted vertices
+ *
+ * @param targetVertex The vertex whose index to be found
+ * @param setOfSortedVertices The set of all sorted vertices
+ * @return The index of targetVertex or -1 if not found
+ */
+int getVertexIndex(const int targetVertex, const set<int> setOfSortedVertices) {
+    if (setOfSortedVertices.empty()) return -1;
     int targetVertexIdx = 0;
-    for (auto vertex : listOfSortedVertices) {
+    for (auto vertex : setOfSortedVertices) {
         // Vertex is unique
         if (vertex == targetVertex) return targetVertexIdx;
         targetVertexIdx++;
@@ -12,7 +18,13 @@ int getVertexIndex(const int targetVertex, const set<int> listOfSortedVertices) 
     return -1;
 }
 
-
+/**
+ * Calculate the sorted set of vertices from an input graph
+ *
+ * @param graph The input graph
+ * @param numEdges The number of edges in the graph
+ * @return The set of sorted vertices
+ */
 set<int> getSetOfSortedVertices(int graph[][3], int numEdges) {
     // Extract unique values, set sorts the elements in ascending order by default
     set<int> setOfSortedVertices;
@@ -24,8 +36,15 @@ set<int> getSetOfSortedVertices(int graph[][3], int numEdges) {
 }
 
 
-/* Perform one step of Bellman-Ford
-*/
+/**
+ * Calculate 1 step of Bellman-Ford on an input graph
+ *
+ * @param graph The input graph
+ * @param numEdges The number of edges in the graph
+ * @param startVertex The start vertex of the Bellman-Ford algorithm
+ * @param BFValue Current Bellman-Ford Value array (to be updated)
+ * @param BFPrev Current Bellman-Ford Previous array (to be updated)
+ */
 void BF(int graph[][3], int numEdges, char startVertex, int BFValue[], int BFPrev[]) {
     if (numEdges <= 0) return; // No edges to process
    
@@ -68,6 +87,15 @@ void printBFPrev(int BFPrev[], int n) {
 #endif
 
 
+/**
+ * Return a string containing the BellmanFord path from input start and goal vertices 
+ *
+ * @param graph The input graph
+ * @param numEdges The number of edges in the graph
+ * @param startVertex The start vertex of the path
+ * @param goalVertex The goal vertex of the path
+ * @return A string containing the shortest path from Start vertex to Goal vertex
+ */
 string BF_Path(int graph[][3], int numEdges, char startVertex, char goalVertex) {
     if (numEdges <= 0) return ""; // No edges to process
     else if (startVertex == goalVertex) return string(1, startVertex);
